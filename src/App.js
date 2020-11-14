ca00bceddf1ef5afc193d3f1f1dbd33fa47335dd
Hossein <djalicrt@gmail.com>
2020-11-14 19:40:15 +0330

    First Part: impl. SearchBox (CSS + Assests)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+const api = {
+  key: "5a873b6b606d9e2aa79aff040a0e7d63",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     <main>
+       <div className="search-box">
+        <input
+        type="text"
+        className="search-bar"
+        placeholder="Seacrh..."
+        />
+       </div>
+     </main>
     </div>
   );
 }
